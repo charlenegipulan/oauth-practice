f77475d5c97bef61169630c8751c62b238feaf4e
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-02 12:41:09 -0700

    define routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 var router = require('express').Router();
+var passport = require('passport');
 
-// The root route renders our only view
+// The root route renders only view
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { user: req.user }); //login/logout UI
+});
+
+// Google 0Auth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+// Google 0Auth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failure: '/'
+  }
+));
+
+// 0Auth logout route
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
