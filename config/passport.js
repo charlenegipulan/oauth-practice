a68db62391628bc8947c0336dd1d70fa1c942a82
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-02 12:32:40 -0700

    define passport methods

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,38 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var Student = require('../models/student');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+}, function(acessToken, refreshToken, profile, cb) {
+    Student.findOne({ googleId: profile.id }, function(err, student) {
+        if (err) return cb(err);
+        if (student) {
+            return cb(null, student);
+        } else {
+          var newStudent = new Student({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id
+          });
+          newStudent.save(function(err) {
+            if (err) return cb(err);
+            return cb(null, newStudent);
+          });
+        }
+    });
+}));
+
+//when someone just logs in, only called once
+passport.serializeUser(function(student, done) {
+    done(null, student.id);
+});
+
+
+passport.deserializeUser(function(id, done) {
+    Student.findById(id, function(err, student) {
+        done(err, student);
+    });
+});
--- models/student.js
+++ models/student.js
@@ -13,7 +13,8 @@ var studentSchema = new mongoose.Schema({
   email: String,
   cohort: String,
   avatar: String,
-  facts: [factSchema]
+  facts: [factSchema],
+  googleId: String
 }, {
   timestamps: true
 });
